19b194de4d4a3eea4423f9efd45c8a7e69932b70
Ricardo Granja Chávez <ricardo.granja@nextia.mx>
2021-08-11 01:27:44 -0500

    Menu creation

---
+++ testnative/ViewModels/MenuViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewModel.swift
+//  testnative
+//
+//  Created by Ricardo Granja Chávez on 11/08/21.
+//
+
+import Foundation
+import UIKit
+
+struct MenuViewModel {
+    let image: UIImage
+    let type: MenuType
+}
+
+enum MenuType: String {
+    case label = "Label"
+}
